3152c1919df5cbafcd9165040919d912ca695c2f
Robson <robsonmoura970@gmail.com>
2023-10-11 11:26:44 -0300

    Adicionando a classe controller definitiva do projeto

---
+++ src/main/java/br/com/robsonmoura/todolist/user/UserController.java
@@ -0,0 +1,20 @@
+package br.com.robsonmoura.todolist.user;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    /**
+     * Estes parametros que o método está solicitando virão diretamente do
+     * body da requisição, por isso utilizamos o @RequestBody
+     *
+     */
+    @PostMapping("/")
+    public void create (@RequestBody UserModel userModel){
+        System.out.println(userModel.name);
+    }
+}
---
+++ src/main/java/br/com/robsonmoura/todolist/user/UserModel.java
@@ -0,0 +1,8 @@
+package br.com.robsonmoura.todolist.user;
+
+public class UserModel{
+
+    public String userName;
+    public String name;
+    public String password;
+}
