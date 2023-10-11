cd5f02d3100e54298ed7103f84b269ebbee2ff82
Robson <robsonmoura970@gmail.com>
2023-10-11 10:31:45 -0300

    first commit

---
+++ src/test/java/br/com/robsonmoura/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.robsonmoura.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
